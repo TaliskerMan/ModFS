92a1cee872de16a7cecea0d4c062c4579ab7fba1
TaliskerMan <38320370+TaliskerMan@users.noreply.github.com>
2026-06-24 15:53:16 -0500

    Auto-sync update

--- test_init.dart
+++ test_init.dart
@@ -3,7 +3,7 @@ import 'lib/ffi.dart';
 
 void main() async {
   print("Start");
-  final libPath = '/home/freecode/antigrav/ModFS/src/libmodfs_core.so';
+  final libPath = 'src/libmodfs_core.so';
   var modfs = ModFSBindings(libPath);
   print("Bindings created");
   var dbPtr = modfs.createDatabase(['/'], [], false);
